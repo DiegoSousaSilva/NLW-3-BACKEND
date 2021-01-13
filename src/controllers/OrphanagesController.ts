159cc70f487f196e6055ef75f61ef0427e14228e
Diego Sousa <derickbenji2@gmail.com>
2021-01-13 00:41:00 -0300

    Trabalhando com Views

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -2,23 +2,29 @@ import {Request, Response} from 'express';
 import {getRepository} from 'typeorm';
 import Orphanage from '../models/Orphanage';
 
+import orphanageView from '../views/orphanages_views';
+
 export default {
 
     async index(req: Request, res: Response){
         const orphanageRepository = getRepository(Orphanage);
 
-        const orphanages = await orphanageRepository.find();
+        const orphanages = await orphanageRepository.find({
+            relations: ['images']
+        });
 
-        return res.json(orphanages);
+        return res.json(orphanageView.renderMany(orphanages));
     },
 
     async show(req: Request, res: Response){
         const {id} = req.params;
         const orphanageRepository = getRepository(Orphanage);
 
-        const orphanage = await orphanageRepository.findOneOrFail(id);
+        const orphanage = await orphanageRepository.findOneOrFail(id, {
+            relations: ['images']
+        });
 
-        return res.json(orphanage);
+        return res.json(orphanageView.render(orphanage));
     },
 
     async create(req: Request, res: Response){
