f4393af77154e64f42ff7d4bede99ad7a7da97fe
Diego Sousa <derickbenji2@gmail.com>
2021-01-13 00:09:37 -0300

    Upload de imagens

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -33,6 +33,13 @@ export default {
           } = req.body;
         
           const orphanageRepository = getRepository(Orphanage);
+
+          const requestImages = req.files as Express.Multer.File[];
+
+          const images = requestImages.map(image =>{
+              return {path: image.filename} 
+          })
+
         
           const orphanage = orphanageRepository.create({
             name,
@@ -41,7 +48,8 @@ export default {
             about,
             instructions,
             opening_hours,
-            open_on_weekends
+            open_on_weekends,
+            images
           });
         
          await orphanageRepository.save(orphanage);
