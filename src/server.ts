37dab03cbed8aa987c2d66c2d10a25c2381998c2
Diego Sousa <derickbenji2@gmail.com>
2021-01-13 00:54:24 -0300

    lidando com exceções

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import express, { response } from 'express';
+import express from 'express';
 import path from 'path';
+import 'express-async-errors';
 
 import routes from './routes';
 
 import './database/connection';
+import errorHandler from './errors/handler';
 
 
 const app = express();
@@ -11,6 +13,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
-
+app.use(errorHandler);
 
 app.listen(3333);
