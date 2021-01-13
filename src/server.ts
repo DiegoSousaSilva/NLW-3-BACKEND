21fa7f82d7a09d6f0b4674952788c30e6e972abb
Diego Sousa <derickbenji2@gmail.com>
2021-01-12 21:46:30 -0300

    Criando banco de dados e fazendo primeiros cadastros de testes

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
-import express from 'express';
+import express, { response } from 'express';
+
+import './database/connection';
 
 const app = express();
 
@@ -7,5 +9,10 @@ app.use(express.json());
 app.get('/users', (req, res)=>{
   return res.json({message: "Diego dev"});
 });
+
+app.post('/orphanages', (req, res)=>{
+  console.log(req.body);
+  return res.json({message: "Diego Dev"});
+})
 
 app.listen(3333);
