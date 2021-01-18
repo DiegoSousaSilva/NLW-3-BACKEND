bea1ccad1c5aba35887851e695672dbcfedca7b2
Diego Sousa <derickbenji2@gmail.com>
2021-01-18 19:53:50 -0300

    adicionando  função para permitir acesso a api via localhost

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,13 @@ import errorHandler from './errors/handler';
 const app = express();
 
 app.use(express.json());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(errorHandler);
